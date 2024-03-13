59435497d87d3884901a2b207ac69fd1dc992be4
Jason <JasonRSmith32@gmail.com>
2024-03-12 21:35:57 -0400

    Finished Profile Configuration

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,52 @@
 {
     public class Program
     {
+        public static int Sum(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+
+        }
+      
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+
+
         static void Main(string[] args)
         {
+
+            var amountOfFruits = Sum(8, 10, 20);
+            var numberOfCars = Multiply(4, 6, 11);
+            var numberOfStudents = Subtract(100, 35);
+            var amountOfMoney = Divide(2000, 5);
+            var amountOfShirts = Modulus(20, 3);
+
+
+
+
+
+
+            //----------------------exercise 1-------------------------------
             Console.WriteLine("Hello, what is your first name ?");
             var userName = Console.ReadLine();
 
